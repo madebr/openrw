adcf7cd0919127172b9b19931adc6e9e8c9c5624
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-11-17 01:26:38 +0100

    rwcore: sprinkle 'const' around file buffers

--- rwengine/src/loaders/LoaderGXT.cpp
+++ rwengine/src/loaders/LoaderGXT.cpp
@@ -13,18 +13,18 @@ void LoaderGXT::load(GameTexts &texts, const FileContentsInfo &file) {
 
     data += 4;  // TKEY
 
-    std::uint32_t blocksize = *reinterpret_cast<std::uint32_t *>(data);
+    std::uint32_t blocksize = *reinterpret_cast<const std::uint32_t *>(data);
 
     data += 4;
 
     auto tdata = data + blocksize + 8;
 
     for (size_t t = 0; t < blocksize / 12; ++t) {
-        size_t offset = *reinterpret_cast<std::uint32_t *>(data + (t * 12 + 0));
+        size_t offset = *reinterpret_cast<const std::uint32_t *>(data + (t * 12 + 0));
         std::string id(data + (t * 12 + 4));
 
-        GameStringChar *stringSrc =
-            reinterpret_cast<GameStringChar *>(tdata + offset);
+        const GameStringChar *stringSrc =
+            reinterpret_cast<const GameStringChar *>(tdata + offset);
         GameString string(stringSrc);
         texts.addText(id, std::move(string));
     }
